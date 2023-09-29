e0d622ed15f7ca104972f500f69f2fcaf0bd02fb
Gabriela <dev.gabrielaellen@gmail.com>
2023-09-29 20:39:39 -0300

    install packtage

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
-const express = require('express')
+require('dotenv').config();
+const express = require("express");
+const mongoose = require("mongoose");
+const session = require("express-session");
+
 const app = express()
-const port = 3000
+const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+// app.set("view engine", "ejs")
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}`)
 })
+

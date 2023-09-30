31afc5fe12834b4e3990ff241be9a39c4c79c2e8
Gabriela <dev.gabrielaellen@gmail.com>
2023-09-30 18:57:34 -0300

    Add 'createView' and include 'find' in the indexView

--- controllers/students_controller.js
+++ controllers/students_controller.js
@@ -3,8 +3,39 @@ const Student = db.students;
 const Op = db.Sequelize.Op;
 
 const indexView = (req, res) => {
-    res.render("index");
+    Student.findAll()
+      .then(data => {
+        res.render("index", { students: data });
+      })
+      .catch(err => {
+        res.status(500).send({
+          message:
+            err.message || "Some error occurred while retrieving students."
+        });
+      });
 };
+
+const createView = (req, res) => { 
+    console.log(req.body)
+    const student = {
+        name: req.body.name,
+        test_grade_one: req.body.test_grade_one,
+        test_grade_two: req.body.test_grade_two
+    };
+
+    Student.create(student)
+        .then(data => {
+            res.redirect("/");
+        })
+        .catch(err => {
+            res.status(500).send({
+                message:
+                    err.message || "Some error occurred while creating the Student."
+            });
+        });
+};
+
 module.exports = {
     indexView,
+    createView,
 };
--- routes/routes.js
+++ routes/routes.js
@@ -1,9 +1,12 @@
 const studentsController = require("../controllers/students_controller.js");
 
 const express = require('express');
+const students = require("../models/students.js");
 const router = express.Router();
 
 
 router.get("/", studentsController.indexView);
+
+router.post("/add", studentsController.createView);
 
 module.exports = router;

6b6dcaa5790f4a6f074116d19400f1af7af24507
Pedro Thomé <pedro.th.dev@gmail.com>
2023-09-30 22:45:53 -0300

    added functional requirements validations

--- controllers/students_controller.js
+++ controllers/students_controller.js
@@ -4,23 +4,47 @@ const Op = db.Sequelize.Op;
 
 const indexView = (req, res) => {
     Student.findAll()
-      .then(data => {
-        res.render("index", { students: data });
-      })
-      .catch(err => {
-        res.status(500).send({
-          message:
-            err.message || "Some error occurred while retrieving students."
+        .then(data => {
+            res.render("index", { students: data });
+        })
+        .catch(err => {
+            res.status(500).send({
+                message: err.message || "Some error occurred while retrieving students."
+            });
         });
-      });
 };
 
-const createView = (req, res) => { 
-    console.log(req.body)
+const createView = (req, res) => {
+  
+    const testGradeOne = parseFloat(req.body.test_grade_one);
+    const testGradeTwo = parseFloat(req.body.test_grade_two);
+
+    if (isNaN(testGradeOne) || isNaN(testGradeTwo)) {
+        return res.status(400).send({ message: "Invalid test grades provided." });
+    }
+
+    if ((testGradeOne || testGradeTwo) < 0  || (testGradeOne || testGradeTwo) > 10) {
+      return res.status(400).send({ message: "Test grades must be between 0 and 10." });
+  }
+
+    let situation;
+    const media = (testGradeOne + testGradeTwo) / 2;
+
+    if (media >= 7) {
+        situation = "aprovado";
+    } else if (media >= 4) {
+        situation = "recuperacao";
+    } else {
+        situation = "reprovado";
+    }
+
     const student = {
+        id: req.body.id,
         name: req.body.name,
-        test_grade_one: req.body.test_grade_one,
-        test_grade_two: req.body.test_grade_two
+        test_grade_one: testGradeOne,
+        test_grade_two: testGradeTwo,
+        average_test_grade: media.toFixed(2),
+        situation,
     };
 
     Student.create(student)
@@ -29,8 +53,7 @@ const createView = (req, res) => {
         })
         .catch(err => {
             res.status(500).send({
-                message:
-                    err.message || "Some error occurred while creating the Student."
+                message: err.message || "Some error occurred while creating the Student."
             });
         });
 };

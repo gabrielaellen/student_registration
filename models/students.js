0d82c166d2275b18acbc689a46c782e59f0530ea
Pedro Thomé <pedro.th.dev@gmail.com>
2023-09-30 22:48:10 -0300

    add average_test_grade and situation atributes

--- models/students.js
+++ models/students.js
@@ -1,13 +1,28 @@
 module.exports = (sequelize, Sequelize) => {
     const Student = sequelize.define("students", {
       name: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
+        allowNull: false
       },
       test_grade_one: {
-        type: Sequelize.DOUBLE
+        type: Sequelize.DOUBLE,
+        validate: {
+          min: 0,
+          max: 10
+      }
       },
       test_grade_two: {
+        type: Sequelize.DOUBLE,
+        validate: {
+          min: 0,
+          max: 10
+      }
+      },
+      average_test_grade: {
         type: Sequelize.DOUBLE
+      },
+      situation: {
+        type: Sequelize.STRING
       }
     });
   
